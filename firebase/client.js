b73b4951c1ce9fcd23371d3f93bc598a46cfe89e
Franco Dionofrio <fadionofrio@gmail.com>
2022-11-11 12:14:31 -0300

    Introduciendo las variables de entorno

--- firebase/client.js
+++ firebase/client.js
@@ -1,11 +1,29 @@
 import { initializeApp } from 'firebase/app';
-import dotenv from 'dotenv'
-import fs from 'fs'
+import dotenv from 'dotenv';
+import fs from 'fs';
 
-dotenv.config()
+dotenv.config();
 
-const firebaseConfig = process.env?.['firebase-keys']
+const {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+};
 
 export const app = initializeApp(firebaseConfig);
-
-
